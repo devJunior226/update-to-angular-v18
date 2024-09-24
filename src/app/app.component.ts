c4d52c859f02702ef531235c4dd67815d41cb843
RAMDE <ramde266@gmail.com>
2024-09-24 13:19:30 +0000

    Value-based and Observable-based reactivity.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    template: `
+    <div class="bg">
+        <h2>La réactivité “Observable-based”</h2>
+        <p>Counter: {{ count$ | async }}</p>
+        <button (click)="increment()">Increment</button>
+    </div>
+    `,
+
+    styles: [`
+        .bg {
+            background: #0000ff;
+            padding: 2rem;
+            color: white;
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+    `]
 })
 export class AppComponent {
     name: string = "Pikachu";
     life: number = 21;
+
+    // 1. La réactivité “Value-based”
+    // count: number = 0;
+    // doCount() {
+    //     this.count++;
+    // }
+
+    // 2. La réactivité “Observable-based”
+    private countSubject = new BehaviorSubject<number>(0);
+    count$: Observable<number> = this.countSubject.asObservable();
+    increment() {
+        this.countSubject.next(this.countSubject.value + 1);
+    }
+
 
     ngOnInit(): void {
         let a_0: number = 1;
@@ -17,7 +47,6 @@ export class AppComponent {
         let a_2 = a_0 + a_1;
         console.log(a_2);
     }
-
 
     incrementLife() {
         this.life = this.life + 1;
