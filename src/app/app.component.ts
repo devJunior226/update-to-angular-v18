0ddf093af1c5b74fe1e342516ace31333f17a35e
RAMDE <ramde266@gmail.com>
2024-09-24 15:28:18 +0000

    Signals, display pokemon name and life.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,21 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [],
-    template: `
-    <div class="bg">
-        <h2>Les Signals</h2>
-        <p>Current value: {{ secondCounter() }}</p>
-        <!-- <p>Double value: {{ counterDouble() }}</p> -->
-        <button class="btn" (click)="incrementAgain()">
-            Increment
-        </button>
-        <button class="btn" (click)="resetAgain()">
-            Reset
-        </button>
-    </div>
-    `,
-
-    styles: [`
-        .bg {
-            background: #0000ff;
-            padding: 2rem;
-            color: white;
-            font-family: Arial, sans-serif;
-            text-align: center;
-        }
-
-        .btn {
-           padding: .5rem 2rem;
-           font-size: 1rem;
-           cursor: pointer;
-        }
-    `]
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    name = signal('Pikachu');
+    life = signal(21);
 
-    // Les signals sans etats-derivés
-    counter = signal(0);
-    counterDouble = computed(() => this.counter() * 2);
-
-    increment() {
-        this.counter.update(n => n + 1);
+    incrementLife() {
+        this.life.update(n => n + 1);
     }
 
-    reset() {
-        this.counter.set(0);
-    }
-
-    // Gestion des effets de bord
-    secondCounter = signal(0);
-
-    constructor() {
-        // Will execute when a signal will change.
-        effect(() => {
-            console.log(`Le compteur a été mis à jour : ${this.secondCounter()}`);
-        })
-    }
-
-
-    incrementAgain() {
-        this.secondCounter.update(n => n + 1);
-    }
-
-    resetAgain() {
-        this.secondCounter.set(0);
+    decrementLife() {
+        this.life.set(21);
     }
 }
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,68 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="bg">
+        <h2>Les Signals</h2>
+        <p>Current value: {{ secondCounter() }}</p>
+        <!-- <p>Double value: {{ counterDouble() }}</p> -->
+        <button class="btn" (click)="incrementAgain()">
+            Increment
+        </button>
+        <button class="btn" (click)="resetAgain()">
+            Reset
+        </button>
+    </div>
+    `,
+  styles: [
+    `
+      .bg {
+        background: #0000ff;
+        padding: 2rem;
+        color: white;
+        font-family: Arial, sans-serif;
+        text-align: center;
+      }
+
+      .btn {
+        padding: 0.5rem 2rem;
+        font-size: 1rem;
+        cursor: pointer;
+      }
+    `,
+  ],
+})
+export class DemoComponent {
+  // Les signals sans etats-derivés
+  counter = signal(0);
+  counterDouble = computed(() => this.counter() * 2);
+
+  increment() {
+    this.counter.update((n) => n + 1);
+  }
+
+  reset() {
+    this.counter.set(0);
+  }
+
+  // Gestion des effets de bord
+  secondCounter = signal(0);
+
+  constructor() {
+    // Will execute when a signal will change.
+    effect(() => {
+      console.log(`Le compteur a été mis à jour : ${this.secondCounter()}`);
+    });
+  }
+
+  incrementAgain() {
+    this.secondCounter.update((n) => n + 1);
+  }
+
+  resetAgain() {
+    this.secondCounter.set(0);
+  }
+}
