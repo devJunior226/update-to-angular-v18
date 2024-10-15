8ccdf85c7c55daaa480fec4d7af97acb1c0eb9d2
Juldas RAMDE <ramde266@gmail.com>
2024-10-15 07:01:39 +0000

    Affichage dynamique et directive structurelle.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, computed, signal } from '@angular/core';
 import { DemoComponent } from './demo/demo.component';
+import { POKEMON_LIST } from './models/pokemon-list.fake';
+import { Pokemon } from './models/pokemon.model';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,24 @@ import { DemoComponent } from './demo/demo.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  pokemons = signal(POKEMON_LIST);
+
+  size(pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+    if (pokemon.life >= 25) {
+      return 'Grand';
+    } else {
+      return 'Moyen';
+    }
+  }
+
+  incrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life + 1;
+  }
+
+  decrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life - 1;
+  }
 }
